a9ee2e149cfeae33be5bca6fe466dce2b83f346a
Shibin Shibin <shibinshibin@Shibins-MacBook-Pro.local>
2024-05-21 21:00:52 +0530

    todo

--- src/pages/Todo/TodoForm.jsx
+++ src/pages/Todo/TodoForm.jsx
@@ -1,34 +1,41 @@
-import React, { useState,useId } from 'react'
+import React, { useState, useId } from 'react'
 import { v4 as uuidv4 } from 'uuid';
-export default function TodoForm() {
-  const [todo,setTodo]=useState('');
-  const tempId=useId();
-  const addTodo =(e)=>{
-    let oldStorage =localStorage.getItem('todo');
-    let newTodo ={
-      id:uuidv4(),
-      task:todo
-    }
-    if(oldStorage==null){
-      localStorage.setItem('todo',JSON.stringify([newTodo]))
-    }else{
-      let parsedTodo =JSON.parse(oldStorage)
-      let newList =[...parsedTodo,newTodo]
-      console.log(newList);
-      localStorage.setItem('todo',JSON.stringify(newList))
-    }
-    // console.log(oldStorage);
-    // console.log(oldStorage);
+export default function TodoForm({tiggerAction}) {
+  const [todo, setTodo] = useState('');
+  const tempId = useId();
+  const addTodo = (e) => {
+    e.preventDefault()
+    if (todo) {
+      let oldStorage = localStorage.getItem('todo');
+      let newTodo = {
+        id: uuidv4(),
+        task: todo
+      }
+      if (oldStorage == null) {
+        localStorage.setItem('todo', JSON.stringify([newTodo]))
+      } else {
+        let parsedTodo = JSON.parse(oldStorage)
+        let newList = [...parsedTodo, newTodo]
+        console.log(newList);
+        localStorage.setItem('todo', JSON.stringify(newList))
+        setTodo('')
+      }
+      tiggerAction()
+      // console.log(oldStorage);
+      // console.log(oldStorage);
 
-    // localStorage.setItem('todo',)
+      // localStorage.setItem('todo',)}
+    }else{
+      alert('Enter Task')
+    }
   }
   return (
     <div className='todo-form'>
-      {/* <form> */}
-          <label>Task name</label>
-          <input type='text' value={todo} onChange={(e)=>setTodo(e.target.value)}/>
-          <button onClick={addTodo}>Add to list</button>
-      {/* </form> */}
+      <form>
+        <label>Task name</label>
+        <input type='text' value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button onClick={addTodo}>Add to list</button>
+      </form>
     </div>
   )
 }
--- src/pages/Todo/TodoList.jsx
+++ src/pages/Todo/TodoList.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import TodoForm from './TodoForm'
 
 export default function TodoList() {
-  return (
-    <div>
-        <table>
-            <thead>
-                <th>#</th>
-                <th>Task</th>
-                <th>Action
-                    <Link to="/add-todo"><button>Add</button></Link>
-                </th>
-            </thead>
-            <tbody>
-                <td>1</td>
-                <td>task 1</td>
-                <td>
-                    <button>View</button>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </td>
-            </tbody>
-        </table>
-    </div>
-  )
+    const [allTodo, setAllTodo] = useState([])
+    const [triggerTodo, setTriggerTodo] = useState(true)
+    useEffect(() => {
+        getdata()
+    }, [triggerTodo])
+    const getdata = () => {
+        let oldStorage = localStorage.getItem('todo');
+        if (oldStorage != null) {
+            let todtoArr = JSON.parse(oldStorage)
+            setAllTodo(todtoArr);
+        }
+    }
+    const tiggerAction = () => {
+        setTriggerTodo(!triggerTodo)
+    }
+    return (
+        <div>
+            <TodoForm tiggerAction={tiggerAction} />
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Task</th>
+                        <th>Action
+                            <Link to="/add-todo"><button>Add</button></Link>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allTodo.map((itm, i) => (
+                        <tr key={itm.id}>
+                            <td>{i + 1}</td>
+                            <td>{itm.task}</td>
+                            <td>
+                                <button>View</button>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+        </div>
+    )
 }
